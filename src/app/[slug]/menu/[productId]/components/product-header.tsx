cc83febde671025c0fd10851eb7ed05b0af180b9
KathrynOliveira <k.olliver18@gmail.com>
2026-01-19 10:21:54 -0300

    feat: add order navigation to product and restaurant headers

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -3,38 +3,46 @@
 import { Product } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
-interface ProductHeaderProps { 
-    product: Pick<Product, "name" | "imageUrl">;
+interface ProductHeaderProps {
+  product: Pick<Product, "name" | "imageUrl">;
 }
 
 const ProductHeader = ({ product }: ProductHeaderProps) => {
-    const router = useRouter();
-    const handleBackClick = () => router.back(); // voltar para a página anterior
+  const router = useRouter();
+  const handleBackClick = () => router.back(); // voltar para a página anterior
+  const { slug } = useParams<{ slug: string }>();
+  const handleOrdersClick = () => router.push(`/${slug}/orders`); // ir para a página de pedidos
 
-    return ( 
-         <div className="relative w-full min-h-[300px]">
-            <Button
-                variant="secondary"
-                size="icon"
-                className="absolute left-4 top-4 z-50 rounded-full"
-                onClick={handleBackClick}
-            >
-                <ChevronLeftIcon />
-            </Button>
-            <Image src={product.imageUrl} alt={product.name} fill className="object-contain"/>
-            <Button
-                variant="secondary"
-                size="icon"
-                className="absolute right-4 top-4 z-50 rounded-full"
-            >
-                <ScrollTextIcon />
-            </Button>
-        </div>
-     );
-}
- 
+  return (
+    <div className="relative min-h-[300px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain"
+      />
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+        onClick={handleOrdersClick}
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  );
+};
+
 export default ProductHeader;
